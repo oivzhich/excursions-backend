35f9ba1766037c08460183b0a0e6f237a74df57f
Olga Ivzhichenko <olga.ivzhichenko@kaltura.com>
2022-06-07 22:16:53 +0200

    Initial commit

---
+++ src/main/java/com/excursions/excursionsbackend/ExcursionsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.excursions.excursionsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcursionsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcursionsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excursions/excursionsbackend/ExcursionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excursions.excursionsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcursionsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
